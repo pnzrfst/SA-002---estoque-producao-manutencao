f15b44ed5f005de37f5a248de09703d26322bdb0
LUIZ CARLOS PEDRO <luiz_c_pedro@estudante.sc.senai.br>
2024-11-18 21:43:01 -0300

    subindo

--- my-app/src/views/cadastro/preloadCadastro.ts
+++ my-app/src/views/cadastro/preloadCadastro.ts
@@ -2,5 +2,7 @@ import "./index.css"
 import { hash } from "bcrypt"
 
 document.getElementById("cadastrar-user").addEventListener("click", async(event: MouseEvent) => {
+    event.preventDefault()
 
+    
 })
